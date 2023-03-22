594fc941121f97cd3ee530ee8c82866830127a5d
Lvll3gliic <lvll3gliic@gmail.com>
2023-03-22 15:21:40 +0200

    API request functions completed, now we can get all the data we need to make app

--- api/api.js
+++ api/api.js
@@ -1,20 +1,52 @@
-import axios from 'axios'
 
 
 
-export const getCategoryList = () =>{
-        return fetch("https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list")
-        .then(response => response.json())
-        .then(data=>data.drinks)
-        .catch(error => console.log(error))
+export const getCategoryList = async () =>{
+        try {
+        const response = await fetch("https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list")
+        const data = await response.json()
+        return data.drinks
+    } catch (error) {
+        return console.log(error)
+    }
     
 }
 
-export const getFullInfoById = (id) =>{
-    return fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`)
-    .then(response => response.json())
-    .then(data=>data.drinks[0])
-    .catch(error => console.log(error))
+export const getFullInfoById = async (id) =>{
+    try {
+        const response = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`)
+        const data = await response.json()
+        return data.drinks[0]
+    } catch (error) {
+        return console.log(error)
+    }
+}
 
+export const getRandomDrink = async () =>{
+    try {
+        const response = await fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php")
+        const data = await response.json()
+        return data.drinks[0]
+    } catch (error) {
+        return console.log(error)
+    }
+}
+export const getCocktailsByName = async (name) =>{
+    try {
+        const response = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${name}`)
+        const data = await response.json()
+        return data.drinks
+    } catch (error) {
+        return console.log(error)
+    }
+}
+export const getCocktailsByCategory = async (category) =>{
 
+    try {
+        const response = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=${category}`)
+        const data = await response.json()
+        return data.drinks
+    } catch (error) {
+        return console.log(error)
+    }
 }
